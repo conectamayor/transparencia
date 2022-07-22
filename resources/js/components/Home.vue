2b17cb2a8e5ee1ba350f43fa7d9917e5bdce6ff7
emkrypted <jesuscova@jisparking.cl>
2022-07-22 06:58:53 -0400

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -271,7 +271,7 @@
         :interval="4000"
         background="#ababab"
         img-width="1024"
-        img-height="480"
+        img-height="200"
         style="text-shadow: 1px 1px 2px #333;"
         @sliding-start="onSlideStart"
         @sliding-end="onSlideEnd"
