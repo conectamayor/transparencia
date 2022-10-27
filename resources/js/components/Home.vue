262cf83848b919508abf8f5034feca46c1f491bb
emkrypted <jesuscova@jisparking.cl>
2022-10-27 16:18:52 -0300

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -252,18 +252,6 @@
             >
           </template>
         </b-carousel-slide>
-        <b-carousel-slide>
-          <template #img>
-            <img
-              class="d-block img-fluid w-100"
-              width="1024"
-              height="480"
-              :src="'./frontend/assets/images/social_impact/Grupo 104.jpg'"
-              alt="image slot"
-            >
-          </template>
-        </b-carousel-slide>
-      </b-carousel>
     </div>
     </b-collapse>
     <div class="section pt-5 pb-5 gray_ligth_section">
