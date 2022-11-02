46077067970fad94dd12b0592d34080b6e78f4dd
emkrypted <jesuscova@jisparking.cl>
2022-11-02 20:04:43 -0300

    cambios

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -142,7 +142,7 @@
           </div>
           <div class="row">
             <div class="col-md-12">
-              <video width="100%" controls>
+              <video width="100%" controls autoplay>
                 <source src="https://conectamayor.cl/video_launcher.MP4" type="video/mp4">
                 Your browser does not support the video tag.
               </video>
