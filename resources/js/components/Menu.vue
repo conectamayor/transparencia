9e66567f2e6960f3d403a4c6f25494ad576dcaf2
emkrypted <jesuscova@jisparking.cl>
2022-11-04 07:49:46 -0300

    cambios

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -8,10 +8,10 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
             <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav class="me-auto">
+                <b-navbar-nav class="ml-auto">
                     <b-nav-item href="https://conectamayor.cl">Inicio</b-nav-item>
-                    <b-nav-item href="https://conectamayor.cl/quienes-somos/">Quiénes Somos</b-nav-item>
-                    <b-nav-item href="https://conectamayor.cl/nuestras-iniciativas/">Nuestras Iniciativas</b-nav-item>
+                    <b-nav-item href="https://conectamayor.cl/quienes-somos/">Quiénes somos</b-nav-item>
+                    <b-nav-item href="https://conectamayor.cl/nuestras-iniciativas/">Nuestras iniciativas</b-nav-item>
                     <b-nav-item href="#">Transparencia</b-nav-item>
                     <b-nav-item href="https://conectamayor.cl/contacto-usuario/">Contacto</b-nav-item>
                     <b-nav-item href="https://conectamayor.cl/blog/">Blog</b-nav-item>
@@ -34,7 +34,11 @@
 <style scoped>
 .icons {
     background-color: #000 !important;
-    padding: 30px;
+    padding: 37px;
+    margin-right: 0px !important;
+}
+
+.icons > b-nav-item {
     color: #fff !important;
 }
 .navbar.navbar-dark.bg-dark {
