11b25491c41fb9692729c9b549f356a5663a0152
emkrypted <jesuscova@jisparking.cl>
2022-11-04 07:59:34 -0300

    cambios

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -1,29 +1,55 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="dark">
-            <b-navbar-brand href="#">
-                <img :src="'./frontend/assets/images/logo/logo.png'" height="80" alt="Site Logo" />
-            </b-navbar-brand>
-
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav class="ml-auto" style="margin-right: -50px !important;">
-                    <b-nav-item href="https://conectamayor.cl">Inicio</b-nav-item>
-                    <b-nav-item href="https://conectamayor.cl/quienes-somos/">Quiénes somos</b-nav-item>
-                    <b-nav-item href="https://conectamayor.cl/nuestras-iniciativas/">Nuestras iniciativas</b-nav-item>
-                    <b-nav-item href="#">Transparencia</b-nav-item>
-                    <b-nav-item href="https://conectamayor.cl/contacto-usuario/">Contacto</b-nav-item>
-                    <b-nav-item href="https://conectamayor.cl/blog/">Blog</b-nav-item>
-                </b-navbar-nav>
-
-                <b-navbar-nav class="ml-auto icons">
-                    <b-nav-item href="https://instagram.com/conectamayor/"><i class="fa-brands fa-instagram"></i></b-nav-item>
-                    <b-nav-item href="https://www.facebook.com/ConectaMayor/"><i class="fa-brands fa-facebook"></i></b-nav-item>
-                    <b-nav-item href="https://www.twitter.com/ConectaMayor"><i class="fa-brands fa-twitter"></i></b-nav-item>
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
+        <header id="header">
+            <div id="branding" class="container-fluid px-0">
+                <div class="col-md-12 px-0">
+                    <div class="row position-relative">
+                        <div class="col-10 col-md-3">
+                            <a href="https://conectamayor.cl/" class="custom-logo-link" rel="home" aria-current="page"><img width="300" height="100" src="https://conectamayor.cl/wp-content/uploads/2021/03/cropped-logo.png" class="custom-logo" alt="Fundación Conecta Mayor"></a>			</div>
+                        <div class="col-2 mobile-item">			
+                            <div onclick="openNav()" id="burger-menu">
+                                <div class="bar1"></div>
+                                <div class="bar2"></div>
+                                <div class="bar3"></div>
+                            </div>
+                        </div>
+                        
+                        <div class="mobile-menu col-md-8 align-items-center justify-content-end">
+                            <nav id="menu">
+                            <!--<div id="search"></div>-->
+                            <div class="menu-menu_header-container"><ul id="menu-menu_header" class="menu"><li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2 current_page_item menu-item-24"><a href="https://conectamayor.cl/" aria-current="page">Inicio</a></li>
+            <li id="menu-item-1070" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1070"><a href="https://conectamayor.cl/quienes-somos/">Quiénes somos</a></li>
+            <li id="menu-item-41" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-41"><a href="https://conectamayor.cl/nuestras-iniciativas/">Nuestras iniciativas</a>
+            <ul class="sub-menu">
+                <li id="menu-item-2965" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2965"><a href="https://conectamayor.cl/consejos-mayores/">Consejos Mayores</a></li>
+                <li id="menu-item-2175" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2175"><a href="https://conectamayor.cl/nuestras-iniciativas#cm-virtual">CM Virtual</a></li>
+                <li id="menu-item-2176" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2176"><a href="https://conectamayor.cl/nuestras-iniciativas#centro-ayuda">Central de Acompañamiento</a></li>
+                <li id="menu-item-2177" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2177"><a href="https://www.vocesmayores.cl/">Voces Mayores</a></li>
+                <li id="menu-item-2652" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2652"><a target="_blank" rel="noopener" href="https://lideresmayores.cl">Líderes Mayores</a></li>
+                <li id="menu-item-2213" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2213"><a href="https://conectarparaintegrar.cl/">Seminario</a></li>
+                <li id="menu-item-2178" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2178"><a href="https://conectamayor.cl/nuestras-iniciativas#insumos-alimentos">Kits de Insumos</a></li>
+            </ul>
+            </li>
+            <li id="menu-item-3941" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3941"><a href="http://transparencia.conectamayor.cl/?_ga=2.120440076.528807401.1667555147-1464390241.1660729815">Transparencia</a></li>
+            <li id="menu-item-1193" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1193"><a href="https://conectamayor.cl/contacto-usuario/">Contacto</a></li>
+            <li id="menu-item-97" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-97"><a href="https://conectamayor.cl/blog/">Blog</a></li>
+            <li id="menu-item-2195" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2195"><a href="https://conectamayor.cl/aportar/">Aportar</a></li>
+            </ul></div>			</nav>
+                        </div><div class="mobile-network col-md-1 rigth-navbar px-0">
+                            <a href="https://www.instagram.com/conectamayor/" target="_blank" class="mx-1 icon-social-network">
+                                <i class="fab fa-instagram-square" aria-hidden="true"></i>
+                            </a>
+                            <a href="https://www.facebook.com/ConectaMayor" target="_blank" class="mx-1 icon-social-network">
+                                <i class="fab fa-facebook" aria-hidden="true"></i>
+                            </a>
+                            <a href="https://twitter.com/ConectaMayor" target="_blank" class="mx-1 icon-social-network">
+                                <i class="fab fa-twitter" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </header>
     </div>
 </template>
 
